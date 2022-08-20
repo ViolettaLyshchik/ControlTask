c48c9eb4e69ade4d40dad8b3824eb532ee680f91
Violetta <viola.kotovich@gmail.com>
2022-08-20 17:26:14 +0300

    control nask

---
+++ Task C#/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Написать программу, которая из имеющегося массива строк формирует массив из строк, длинна которых
+меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. */
+
+
+
+string[] array1 = new string[6] {"hello", "-2", "123", "world", "2", ":-)"};
+string[] array2 = new string[array1.Length];
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(array1, array2);
+PrintArray(array2);
